49d4b6a8dea8dd2d9a7b37e0cf73d105f177d14c
magiclake <magic.lake@163.com>
2018-06-16 00:04:44 +0800

    add message ui

---
+++ src/class/Gloable.js
@@ -0,0 +1,12 @@
+/**
+ * Created by m on 2018/6/15.
+ */
+
+export default  {
+    getUserInfo () {
+        return this._userInfo;
+    },
+    setUserInfo (v) {
+        this._userInfo = v;
+    }
+}
